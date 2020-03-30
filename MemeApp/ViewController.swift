50c4fe4739fdec2cca248d87002954fec33a9fb6
David Iriarte <diriarte@condorlabs.io>
2020-03-30 12:51:02 -0500

    Initial Commit

---
+++ MemeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by David Iriarte on 30/03/20.
+//  Copyright © 2020 David Iriarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
